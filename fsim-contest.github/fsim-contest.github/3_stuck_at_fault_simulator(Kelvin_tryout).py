ae35880b69b6bb76f7bb0f0069ef47eeb01b31ab
Kung-Kelvin <kung-gomez-kelvin2604@mail.kyutech.jp>
2025-10-08 11:49:45 +0000

    Adding read bench and read test files Only run "correct" behavior

--- fsim-contest.github/fsim-contest.github/3_stuck_at_fault_simulator(Kelvin_tryout).py
+++ fsim-contest.github/fsim-contest.github/3_stuck_at_fault_simulator(Kelvin_tryout).py
@@ -50,6 +50,87 @@ def logic_gate(gate:str, inputs:list):
     elif gate=="BUFF":
         return 1 if inputs[0] else 0
 
+def read_bench(file: str):
+    '''
+    :param str file: it should be the path of the file to read (.bench)
+    
+    :return dict signal_line: #keys is the signal number #value is the signal logic value
+    :return list circ_struc: this contains the interconnection information between the logic gates
+    :return list primary_input: a list of the primary input indexes of the circuit
+    :return list priamry_output: a list of the primary output indexes of the circuit
+    '''
+    aux=0
+    signal_line={} #key:singal line value: logic value (0/1)
+    primary_input=[]
+    primary_output=[]
+    circ_struc=[] #circuit structure
+    with open(file) as f:
+        while True:
+            a=f.readline().strip()
+            if aux>3:
+                break
+            if not(a):
+                aux+=1
+                continue
+            else:
+                if "#" in a:
+                    continue
+                else:
+                    if 'INPUT' in a or 'OUTPUT' in a: #connections for primary input and primary output
+                        b=''.join(filter(str.isnumeric,a))
+                        signal_line[b]=''
+                        if 'INPUT' in a:
+                            primary_input.append(b)
+                        else:
+                            primary_output.append(b)
+                    else: # signal = gate (input signal)
+                        signal_line[a.split(' = ')[0]]=''
+                        circ_struc.append(a)
+    f.close()
+    log (f"Finished reading bench file: {file}")
+    return signal_line,circ_struc,primary_input,primary_output
+
+def read_test(test_file:str, signal_line:dict, circ_struc:list, primary_input:list,primary_output:list):
+    '''
+    Args:
+        test_file(str): the path of the file to read the data to test (.test)
+        signal_line(dict): contains the "order" of the circuit
+        circ_struc(list): it contains the information of the circuit behavior
+        primary_input(list): the primary inputs of the circuit. It will be used to read the data from the test file
+        primary_output(list): the primary outputs of the circuit.
+
+    Returns:
+        signal_line(dict): it contains the information of the circuit after run
+        circuit_output(list): it contains only the primary output logic value
+    '''
+    #reading the test file for data and testing
+    #this script produces the "correct" behaviour
+    with open(test_file) as f:
+        while True:
+            line=f.readline().strip()
+            if not(line):
+                break
+            else: #I assume the data is organized in the bench stated order
+                #assigning the test data to the corresponding signals
+                for a in range(len(primary_input)):
+                    try:
+                        signal_line[primary_input[a]]=int(line[a])
+                    except:
+                        continue
+                
+                #executing the testing from the bench file (running the circuit)
+                #structure= output signal= logic gate (input signal)
+                for a in circ_struc:
+                    output_signal=a.split(' = ')[0]
+                    gate=a.split(' = ')[1].split('(')[0]
+                    input_signal_index=a.split(' = ')[1].split('(')[1][:-1]
+                    input_signals=[signal_line[x.strip()] for x in input_signal_index.split(',')]
+                    signal_line[output_signal]=logic_gate(gate,input_signals)
+                circuit_output=[signal_line[x] for x in primary_output]
+                # print(f'Data input: {line[:line.index('-')]}, \tData output: {circuit_output}')
+    f.close()
+    log(f"Finished running test file: {test_file}")
+    return signal_line, circuit_output
 
 def main():
 
@@ -63,7 +144,6 @@ def main():
     with open(args.tests, 'r') as file:
         tests = logic.mvarray(*file.read().splitlines())
     log.info(f'TestDataShape {tests.shape}')
-
     #
     # Add fault simulation code
     #
